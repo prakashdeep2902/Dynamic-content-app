413d5fae39b88c46aefcfd72f393f18dc9638236
prakash deep sharma <42887999+prakashdeep2902@users.noreply.github.com>
2023-05-14 21:26:23 +0530

    clear all the unwanted file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => setProducts(data.products))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+
+  const getPageData = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('id');
+    return products.find(product => product.id === parseInt(productId));
+  };
+
+  const pageData = getPageData();
+//  console.log(products)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {pageData ? (
+        <div>
+          <h2>{pageData.title}</h2>
+          <p>{pageData.description}</p>
+          <p>Price: ${pageData.price}</p>
+          <p>Discount: {pageData.discountPercentage}%</p>
+          <p>Rating: {pageData.rating}</p>
+          <p>Stock: {pageData.stock}</p>
+          <p>Brand: {pageData.brand}</p>
+          <p>Category: {pageData.category}</p>
+          <img src={pageData.thumbnail} alt={pageData.title} />
+          {pageData.images.map((image, index) => (
+            <img key={index} src={image} alt={`Image ${index + 1}`} />
+          ))}
+        </div>
+      ) : (
+        <p>Page not found.</p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
